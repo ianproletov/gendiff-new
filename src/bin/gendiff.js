244bea9bac9a0f23feedf5149ff4350fc2e9fb80
ianproletov <yan.proletov@gmail.com>
2020-11-08 21:12:07 +0300

    add default parser and render to gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,13 +5,12 @@ import gendiff from '..';
 
 const program = new Command();
 program
-  .version('0.0.1')
+  .version('0.1.0')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .action((firstConfig, secondConfig, options) => {
-    const [first, second] = gendiff(firstConfig, secondConfig, options.format);
-    console.log(first);
-    console.log(second);
+    const diff = gendiff(firstConfig, secondConfig, options.format);
+    console.log(diff);
   });
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,61 @@
 import path from 'path';
 import fs from 'fs';
-import _ from 'lodash';
+import _, { has } from 'lodash';
+
+const conditions = [
+  {
+    check: (key, data1, data2) => _.has(data1, key) && !_.has(data2, key),
+    action: (value1) => ({ value: value1, type: 'deleted' }),
+  },
+  {
+    check: (key, data1, data2) => !_.has(data1, key) && has(data2, key),
+    action: (value1, value2) => ({ value: value2, type: 'added' }),
+  },
+  {
+    check: (key, data1, data2) => _.get(data1, key) !== _.get(data2, key),
+    action: (value1, value2) => ({ value: value1, updatedValue: value2, type: 'updated' }),
+  },
+  {
+    check: (key, data1, data2) => _.get(data1, key) === _.get(data2, key),
+    action: (value1) => ({ value: value1, type: 'equal' }),
+  },
+];
+
+const parse = (data1, data2) => {
+  const beforeKeys = Object.keys(data1);
+  const afterKeys = Object.keys(data2);
+  const keys = _.union(beforeKeys, afterKeys);
+  return keys.map((key) => {
+    const { action } = conditions.find(({ check }) => check(key, data1, data2));
+    return { key, ...action(data1[key], data2[key]) };
+  });
+};
+
+const typeMap = {
+  equal: (key, value) => `  ${key}: ${value}`,
+  deleted: (key, value) => `- ${key}: ${value}`,
+  added: (key, value) => `+ ${key}: ${value}`,
+  updated: (key, value, updatedValue) => [`+ ${key}: ${updatedValue}`, `- ${key}: ${value}`],
+};
+
+const render = (data, deep = 1) => {
+  const ident = ' '.repeat(deep * 2);
+  const parsedItems = _.flatten(data.map((item) => {
+    const {
+      key,
+      value,
+      updatedValue,
+      type,
+    } = item;
+    return typeMap[type](key, value, updatedValue);
+  }))
+    .map((parsedItem) => `${ident}${parsedItem}`);
+  return `{\n${parsedItems.join('\n')}\n}`;
+};
 
 const getDataFromFile = (filepath) => {
   const absPath = path.resolve(process.cwd(), filepath);
   return fs.readFileSync(absPath, 'utf-8');
-};
-
-const getDiff = (data1, data2) => {
-  const beforeKeys = Object.keys(data1);
-  const afterKeys = Object.keys(data2);
-  return _.union(beforeKeys, afterKeys);
 };
 
 const gendiff = (filepath1, filepath2) => {
@@ -18,8 +63,8 @@ const gendiff = (filepath1, filepath2) => {
   const data2 = getDataFromFile(filepath2);
   const first = JSON.parse(data1);
   const second = JSON.parse(data2);
-  const diffData = getDiff(first, second);
-  return diffData;
+  const diffData = parse(first, second);
+  return render(diffData);
 };
 
 export default gendiff;
