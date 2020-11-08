25e5b359688f224ad2c8f161d26577aa8dd8a974
ianproletov <yan.proletov@gmail.com>
2020-11-08 12:35:03 +0300

    add commander

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('gendiff started');
+import { Command } from 'commander';
+
+const program = new Command();
+program
+  .version('0.0.1')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .action((firstConfig, secondConfig, options) => {
+    console.log(firstConfig, secondConfig, options.format);
+  });
+program.parse(process.argv);
