caeaa474df63de82ce068783dca451a4cb1f31e6
ianproletov <yan.proletov@gmail.com>
2020-11-08 14:52:09 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,13 @@
-import fn from '../src';
+import path from 'path';
+import fs from 'fs';
+import gendiff from '../src';
 
 test('main test', () => {
-  const word = 'ian';
-  const actual = fn(word);
-  const expected = 'IAN';
+  const fixturesPath = '__fixtures__';
+  const beforePath = path.resolve(__dirname, fixturesPath, 'before.json');
+  const afterPath = path.resolve(__dirname, fixturesPath, 'after.json');
+  const expectedPath = path.resolve(__dirname, fixturesPath, 'jsonresult');
+  const actual = gendiff(beforePath, afterPath);
+  const expected = fs.readFileSync(expectedPath, 'utf-8');
   expect(actual).toEqual(expected);
 });
