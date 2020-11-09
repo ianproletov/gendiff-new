08bd7bef73c606317590f5be0cb1e254e208accf
ianproletov <yan.proletov@gmail.com>
2020-11-09 19:13:37 +0300

    add nested test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,16 +2,18 @@ import path from 'path';
 import fs from 'fs';
 import gendiff from '../src';
 
-describe.each(['.json', '.yml', '.ini'])('%s test', (extension) => {
-  const fixturesPath = '__fixtures__';
-  const beforeFileName = `before${extension}`;
-  const afterFileName = `after${extension}`;
-  const beforePath = path.resolve(__dirname, fixturesPath, beforeFileName);
-  const afterPath = path.resolve(__dirname, fixturesPath, afterFileName);
-  const expectedPath = path.resolve(__dirname, fixturesPath, 'jsonresult');
-  const actual = gendiff(beforePath, afterPath);
-  const expected = fs.readFileSync(expectedPath, 'utf-8');
-  test('check', () => {
-    expect(actual).toEqual(expected);
+describe.each(['plain', 'nested'])('test %s files', (complexity) => {
+  const fixturesPath = path.join('__fixtures__', complexity);
+  describe.each(['.json', '.yml', '.ini'])('%s test', (extension) => {
+    const beforeFileName = `before${extension}`;
+    const afterFileName = `after${extension}`;
+    const beforePath = path.resolve(__dirname, fixturesPath, beforeFileName);
+    const afterPath = path.resolve(__dirname, fixturesPath, afterFileName);
+    const expectedPath = path.resolve(__dirname, fixturesPath, 'result');
+    const actual = gendiff(beforePath, afterPath);
+    const expected = fs.readFileSync(expectedPath, 'utf-8');
+    test('check', () => {
+      expect(actual).toEqual(expected);
+    });
   });
 });
