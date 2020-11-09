5fdfca2ba7f7743399a7ee18ffad2fb49295826a
ianproletov <yan.proletov@gmail.com>
2020-11-09 22:23:01 +0300

    add ability for diff renderers test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,18 +2,21 @@ import path from 'path';
 import fs from 'fs';
 import gendiff from '../src';
 
-describe.each(['plain', 'nested'])('test %s files', (complexity) => {
+describe.each(['simple', 'nested'])('test %s files', (complexity) => {
   const fixturesPath = path.join('__fixtures__', complexity);
   describe.each(['.json', '.yml', '.ini'])('%s test', (extension) => {
     const beforeFileName = `before${extension}`;
     const afterFileName = `after${extension}`;
-    const beforePath = path.resolve(__dirname, fixturesPath, beforeFileName);
-    const afterPath = path.resolve(__dirname, fixturesPath, afterFileName);
-    const expectedPath = path.resolve(__dirname, fixturesPath, 'result');
-    const actual = gendiff(beforePath, afterPath);
-    const expected = fs.readFileSync(expectedPath, 'utf-8');
-    test('check', () => {
-      expect(actual).toEqual(expected);
+    describe.each(['default'])('test %s render', (type) => {
+      const beforePath = path.resolve(__dirname, fixturesPath, beforeFileName);
+      const afterPath = path.resolve(__dirname, fixturesPath, afterFileName);
+      const result = `result${type}`;
+      const expectedPath = path.resolve(__dirname, fixturesPath, result);
+      const actual = gendiff(beforePath, afterPath);
+      const expected = fs.readFileSync(expectedPath, 'utf-8');
+      test('check', () => {
+        expect(actual).toEqual(expected);
+      });
     });
   });
 });
